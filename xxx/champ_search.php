61fba34cb9b6e9b0bcb9ba82378322ccfecfe001
ryanDalain <sackohammers1992@gmail.com>
2013-11-29 23:59:51 -0500

    updated ui and search

--- xxx/champ_search.php
+++ xxx/champ_search.php
@@ -1,80 +1,144 @@
 <html>
 <body>
 
-	<?php
-	
-	$db_host = "localhost";
-	$db_username = "root";
-	$db_pass = "";
-	$db_name = "champ select";
-	
-	@mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
-	@mysql_select_db("$db_name") or die ("DB not found");
-	
-	echo "Successfully Connected to DB";
-	?>
+        <?php
+        
+        $db_host = "localhost";
+        $db_username = "root";
+        $db_pass = "";
+        $db_name = "champ select";
+        
+        @mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
+        @mysql_select_db("$db_name") or die ("DB not found");
+        
+        ?>
+
 <br>
 
-	<?php
-	
-	// Set up base query
-	$queryOriginal = "SELECT * FROM Champion";
-		$queryByName = " WHERE Champion.Name = ";
-		$queryByDifficulty = " WHERE Champion.Difficulty = ";
-		$queryByWinRate = "";
-	$queryData = "";
-		
-	// depending on drop-down menu, append original query
-	/*
-	switch($_POST['search_peram'])
+        <?php
+        
+        // Set up base query
+        $queryOriginal = "SELECT Name, Position_Name FROM champion";
+
+	// Create an array, Grab the values from each dropdown menu
+	// champions
+	$arr_champions = array($_POST["champ_dropdown_1"],
+				 $_POST["champ_dropdown_2"],
+				 $_POST["champ_dropdown_3"],
+				 $_POST["champ_dropdown_4"],
+				 $_POST["champ_dropdown_5"]);
+
+	// positions	
+	$arr_positions = array($_POST["position_dropdown_1"],
+				 $_POST["position_dropdown_2"],
+				 $_POST["position_dropdown_3"],
+				 $_POST["position_dropdown_4"],
+				 $_POST["position_dropdown_5"]);
+
+
+	$arr_positions_taken = array("Top"=>false, "Middle"=>false, "Jungle"=>false, "Support"=>false, "Carry"=>false);
+
+	$arrlength = count($arr_positions);
+
+	$queryOriginal = $queryOriginal . ' INNER JOIN position ON champion.Name = position.Champ_Name';
+	$queryOriginal = $queryOriginal . ' WHERE position.isPrimary = 1';
+
+	// Check and exclude Champions selected
+	for($x = 0; $x < $arrlength; $x++)
+	{	
+		if($arr_champions[$x] != "Default")
+		{
+			$queryOriginal = $queryOriginal . ' AND Name <> "' . $arr_champions[$x] . '"';
+		}		
+	}
+
+	// Check which lanes have been selected
+	for($x = 0; $x < $arrlength; $x++)
+	{	
+		switch($arr_positions[$x])
+		{
+		case "Top":
+			if($arr_positions_taken['Top'] == false)
+			{
+				$queryOriginal = $queryOriginal . ' AND Position_Name <> "Top"';
+				$arr_positions_taken['Top'] = true;
+			}
+			break;
+
+		case "Middle":
+			if($arr_positions_taken['Middle'] == false)
+			{
+				$queryOriginal = $queryOriginal . ' AND Position_Name <> "Middle"';
+				$arr_positions_taken['Middle'] = true;
+			}
+			break;
+			
+		case "Jungle":
+			if($arr_positions_taken['Jungle'] == false)
+			{
+				$queryOriginal = $queryOriginal . ' AND Position_Name <> "Jungle"';
+				$arr_positions_taken['Jungle'] = true;
+			}
+			break;
+
+		case "Support":
+			if($arr_positions_taken['Support'] == false)
+			{
+				$queryOriginal = $queryOriginal . ' AND Position_Name <> "Bot Support"';
+				$arr_positions_taken['Support'] = true;
+			}
+			break;
+
+		case "Carry":
+			if($arr_positions_taken['Carry'] == false)
+			{
+				$queryOriginal = $queryOriginal . ' AND Position_Name <> "Bot Carry"';
+				$arr_positions_taken['Carry'] = true;
+			}
+			break;
+		}		
+	}
+
+	$queryOriginal = $queryOriginal . " ORDER BY position.Position_Name ASC";
+
+        echo $queryOriginal . "<br>";        
+	echo "<br>";
+	echo "Open Positions: " . "<br>";
+
+	foreach($arr_positions_taken as $x => $x_value)
 	{
-		case "Name":
-			echo "You Wanted to search by name!<br>";
-			$queryOriginal .= $queryByName;
-			break;
-		case "Difficulty":
-			$queryOriginal .= $queryByDifficulty;
-			echo "You Wanted to search by Difficulty!<br>";
-			break;
-		case "Win Rate":
-			// flexibility of win rate within 0.1
-			$queryByWinRate .= " WHERE Champion.Win_Rate > '{$_POST['input_box']}' - 0.1 AND
-										Champion.Win_Rate < '{$_POST['input_box']}' + 0.1";
-			$queryOriginal .= $queryByWinRate;
-			echo "You Wanted to search by Win Rate!<br>";
-			break;
-		default:
-			echo "Could not resolve switch case<br>";
-			break;
+		if($x_value == false)
+		{
+			echo $x . " ";
+		}
 	}
-	*/
-	// echo what the final query is
 	
-	echo $queryOriginal . "<br>";
-	
+	echo "<br>";
+
 	// now query the database
-	$query_data = mysql_query($queryOriginal) or die("Could not find Champion table");
-	
-	// formatting for table
-		// table column headings
-	Print "<table border cellpadding=3>"; 
-	Print "<th>Name</th>";
-	Print "<th>Difficulty</th>";
-	Print "<th>Win Rate</th>";
-	while($info = mysql_fetch_array( $query_data )) 
-	{ 
-		// table row
-		Print "<tr>";
-		// table data
-		Print "<td>" . $info['Name'] . "</td> "; 
-		Print "<td>" . $info['Difficulty'] . " </td>"; 
-		Print "<td>" . $info['Win_Rate'] . "% </td>"; 
-		// end table row
-		Print "</tr>";
-	}
-	Print "</table>"; 
-	
-	?>
+        $query_data = mysql_query($queryOriginal) or die("Could not find Champion table");
+        
+        // formatting for table
+        // table column headings
+        Print "<table border cellpadding=3>"; 
+        Print "<th>Name</th>";
+	Print "<th>Position</th>";
+
+        while($info = mysql_fetch_array( $query_data )) 
+        { 
+                // table row
+                Print "<tr>";
+
+                // table data
+                Print "<td>" . $info['Name'] . "</td> "; 
+                Print "<td>" . $info['Position_Name'] . "</td> "; 
+
+                // end table row
+                Print "</tr>";
+        }
+        Print "</table>"; 
+        
+        ?>
 <br>
 
 
--- xxx/index.php
+++ xxx/index.php
@@ -133,10 +133,19 @@ Select Champion by:
 </form>
 -->
 
-<form action="champ_search.php" target="resultsFrame" method="post">
+<form action="champ_search.php" method="post" target="resultsFrame">
 	<table>
 		
+		<tr>
+			<td>Question</td>
+			<td>Team mate #1</td>
+			<td>Team mate #2</td>
+			<td>Team mate #3</td>
+			<td>Team mate #4</td>
+			<td>Team mate #5</td>
+		</tr>
 		<tr> <!--Row 1-->
+			<td>Do you know any team mates?</td>
 			<td>
 			<select name="champ_dropdown_1">
 				<option value="Default">Select Champ</option>
@@ -622,131 +631,132 @@ Select Champion by:
 			</select>
 			</td>
 			<td>
-				<select name="champ_dropdown_5">
-					<option value="Default">Select Champ</option>
-					<option value="Aatrox">Aatrox</option>
-					<option value="Ahri">Ahri</option>
-					<option value="Akali">Akali</option>
-					<option value="Alistar">Alistar</option>
-					<option value="Amumu">Amumu</option>
-					<option value="Anivia">Anivia</option>
-					<option value="Annie">Annie</option>
-					<option value="Ashe">Ashe</option>
-					<option value="Blitzcrank">Blitzcrank</option>
-					<option value="Brand">Brand</option>
-					<option value="Caitlyn">Caitlyn</option>
-					<option value="Cassiopeia">Cassiopeia</option>
-					<option value="Cho'Gath">Cho'Gath</option>
-					<option value="Corki">Corki</option>
-					<option value="Darius">Darius</option>
-					<option value="Diana">Diana</option>
-					<option value="Dr. Mundo">Dr. Mundo</option>
-					<option value="Draven">Draven</option>
-					<option value="Elise">Elise</option>
-					<option value="Evelynn">Evelynn</option>
-					<option value="Ezreal">Ezreal</option>
-					<option value="Fiddlesticks">Fiddlesticks</option>
-					<option value="Fiora">Fiora</option>
-					<option value="Fizz">Fizz</option>
-					<option value="Galio">Galio</option>
-					<option value="Gangplank">Gangplank</option>
-					<option value="Garen">Garen</option>
-					<option value="Gragas">Gragas</option>
-					<option value="Graves">Graves</option>
-					<option value="Hecarim">Hecarim</option>
-					<option value="Heimerdinger">Heimerdinger</option>
-					<option value="Irelia">Irelia</option>
-					<option value="Janna">Janna</option>
-					<option value="Jarvan IV">Jarvan IV</option>
-					<option value="Jax">Jax</option>
-					<option value="Jayce">Jayce</option>
-					<option value="Jinx">Jinx</option>
-					<option value="Karma">Karma</option>
-					<option value="Karthus">Karthus</option>
-					<option value="Kassadin">Kassadin</option>
-					<option value="Katarina">Katarina</option>
-					<option value="Kayle">Kayle</option>
-					<option value="Kennen">Kennen</option>
-					<option value="Kha'Zix">Kha'Zix</option>
-					<option value="Kog'Maw">Kog'Maw</option>
-					<option value="">LeBlanc</option>
-					<option value="LeBlanc">Lee Sin</option>
-					<option value="Leona">Leona</option>
-					<option value="Lissandra">Lissandra</option>
-					<option value="Lucian">Lucian</option>
-					<option value="Lulu">Lulu</option>
-					<option value="Lux">Lux</option>
-					<option value="Malphite">Malphite</option>
-					<option value="Malzahar">Malzahar</option>
-					<option value="Maokai">Maokai</option>
-					<option value="Master Yi">Master Yi</option>
-					<option value="Miss Fortune">Miss Fortune</option>
-					<option value="Mordekaiser">Mordekaiser</option>
-					<option value="Morgana">Morgana</option>
-					<option value="Nami">Nami</option>
-					<option value="Nasus">Nasus</option>
-					<option value="Nautilus">Nautilus</option>
-					<option value="Nidalee">Nidalee</option>
-					<option value="Nocturne">Nocturne</option>
-					<option value="Nunu">Nunu</option>
-					<option value="Olaf">Olaf</option>
-					<option value="Orianna">Orianna</option>
-					<option value="Pantheon">Pantheon</option>
-					<option value="Poppy">Poppy</option>
-					<option value="Quinn">Quinn</option>
-					<option value="Rammus">Rammus</option>
-					<option value="Renekton">Renekton</option>
-					<option value="Rengar">Rengar</option>
-					<option value="Riven">Riven</option>
-					<option value="Rumble">Rumble</option>
-					<option value="Ryze">Ryze</option>
-					<option value="Sejuani">Sejuani</option>
-					<option value="Shaco">Shaco</option>
-					<option value="Shen">Shen</option>
-					<option value="Shyvana">Shyvana</option>
-					<option value="">Singed</option>
-					<option value="Singed">Sion</option>
-					<option value="Sivir">Sivir</option>
-					<option value="Skarner">Skarner</option>
-					<option value="Sona">Sona</option>
-					<option value="Soraka">Soraka</option>
-					<option value="Swain">Swain</option>
-					<option value="Syndra">Syndra</option>
-					<option value="Talon">Talon</option>
-					<option value="Taric">Taric</option>
-					<option value="Teemo">Teemo</option>
-					<option value="Thresh">Thresh</option>
-					<option value="Tristana">Tristana</option>
-					<option value="Trundle">Trundle</option>
-					<option value="Tryndamere">Tryndamere</option>
-					<option value="Twisted Fate">Twisted Fate</option>
-					<option value="Twitch">Twitch</option>
-					<option value="Udyr">Udyr</option>
-					<option value="Urgot">Urgot</option>
-					<option value="Varus">Varus</option>
-					<option value="Vayne">Vayne</option>
-					<option value="Veigar">Veigar</option>
-					<option value="Vi">Vi</option>
-					<option value="Viktor">Viktor</option>
-					<option value="Vladimir">Vladimir</option>
-					<option value="Volibear">Volibear</option>
-					<option value="Warwick">Warwick</option>
-					<option value="Wukong">Wukong</option>
-					<option value="Xerath">Xerath</option>
-					<option value="Xin Zhao">Xin Zhao</option>
-					<option value="Yorick">Yorick</option>
-					<option value="Zac">Zac</option>
-					<option value="Zed">Zed</option>
-					<option value="Ziggs">Ziggs</option>
-					<option value="Zilean">Zilean</option>
-					<option value="Zyra">Zyra</option>
-				</select>
+			<select name="champ_dropdown_5">
+				<option value="Default">Select Champ</option>
+				<option value="Aatrox">Aatrox</option>
+				<option value="Ahri">Ahri</option>
+				<option value="Akali">Akali</option>
+				<option value="Alistar">Alistar</option>
+				<option value="Amumu">Amumu</option>
+				<option value="Anivia">Anivia</option>
+				<option value="Annie">Annie</option>
+				<option value="Ashe">Ashe</option>
+				<option value="Blitzcrank">Blitzcrank</option>
+				<option value="Brand">Brand</option>
+				<option value="Caitlyn">Caitlyn</option>
+				<option value="Cassiopeia">Cassiopeia</option>
+				<option value="Cho'Gath">Cho'Gath</option>
+				<option value="Corki">Corki</option>
+				<option value="Darius">Darius</option>
+				<option value="Diana">Diana</option>
+				<option value="Dr. Mundo">Dr. Mundo</option>
+				<option value="Draven">Draven</option>
+				<option value="Elise">Elise</option>
+				<option value="Evelynn">Evelynn</option>
+				<option value="Ezreal">Ezreal</option>
+				<option value="Fiddlesticks">Fiddlesticks</option>
+				<option value="Fiora">Fiora</option>
+				<option value="Fizz">Fizz</option>
+				<option value="Galio">Galio</option>
+				<option value="Gangplank">Gangplank</option>
+				<option value="Garen">Garen</option>
+				<option value="Gragas">Gragas</option>
+				<option value="Graves">Graves</option>
+				<option value="Hecarim">Hecarim</option>
+				<option value="Heimerdinger">Heimerdinger</option>
+				<option value="Irelia">Irelia</option>
+				<option value="Janna">Janna</option>
+				<option value="Jarvan IV">Jarvan IV</option>
+				<option value="Jax">Jax</option>
+				<option value="Jayce">Jayce</option>
+				<option value="Jinx">Jinx</option>
+				<option value="Karma">Karma</option>
+				<option value="Karthus">Karthus</option>
+				<option value="Kassadin">Kassadin</option>
+				<option value="Katarina">Katarina</option>
+				<option value="Kayle">Kayle</option>
+				<option value="Kennen">Kennen</option>
+				<option value="Kha'Zix">Kha'Zix</option>
+				<option value="Kog'Maw">Kog'Maw</option>
+				<option value="">LeBlanc</option>
+				<option value="LeBlanc">Lee Sin</option>
+				<option value="Leona">Leona</option>
+				<option value="Lissandra">Lissandra</option>
+				<option value="Lucian">Lucian</option>
+				<option value="Lulu">Lulu</option>
+				<option value="Lux">Lux</option>
+				<option value="Malphite">Malphite</option>
+				<option value="Malzahar">Malzahar</option>
+				<option value="Maokai">Maokai</option>
+				<option value="Master Yi">Master Yi</option>
+				<option value="Miss Fortune">Miss Fortune</option>
+				<option value="Mordekaiser">Mordekaiser</option>
+				<option value="Morgana">Morgana</option>
+				<option value="Nami">Nami</option>
+				<option value="Nasus">Nasus</option>
+				<option value="Nautilus">Nautilus</option>
+				<option value="Nidalee">Nidalee</option>
+				<option value="Nocturne">Nocturne</option>
+				<option value="Nunu">Nunu</option>
+				<option value="Olaf">Olaf</option>
+				<option value="Orianna">Orianna</option>
+				<option value="Pantheon">Pantheon</option>
+				<option value="Poppy">Poppy</option>
+				<option value="Quinn">Quinn</option>
+				<option value="Rammus">Rammus</option>
+				<option value="Renekton">Renekton</option>
+				<option value="Rengar">Rengar</option>
+				<option value="Riven">Riven</option>
+				<option value="Rumble">Rumble</option>
+				<option value="Ryze">Ryze</option>
+				<option value="Sejuani">Sejuani</option>
+				<option value="Shaco">Shaco</option>
+				<option value="Shen">Shen</option>
+				<option value="Shyvana">Shyvana</option>
+				<option value="">Singed</option>
+				<option value="Singed">Sion</option>
+				<option value="Sivir">Sivir</option>
+				<option value="Skarner">Skarner</option>
+				<option value="Sona">Sona</option>
+				<option value="Soraka">Soraka</option>
+				<option value="Swain">Swain</option>
+				<option value="Syndra">Syndra</option>
+				<option value="Talon">Talon</option>
+				<option value="Taric">Taric</option>
+				<option value="Teemo">Teemo</option>
+				<option value="Thresh">Thresh</option>
+				<option value="Tristana">Tristana</option>
+				<option value="Trundle">Trundle</option>
+				<option value="Tryndamere">Tryndamere</option>
+				<option value="Twisted Fate">Twisted Fate</option>
+				<option value="Twitch">Twitch</option>
+				<option value="Udyr">Udyr</option>
+				<option value="Urgot">Urgot</option>
+				<option value="Varus">Varus</option>
+				<option value="Vayne">Vayne</option>
+				<option value="Veigar">Veigar</option>
+				<option value="Vi">Vi</option>
+				<option value="Viktor">Viktor</option>
+				<option value="Vladimir">Vladimir</option>
+				<option value="Volibear">Volibear</option>
+				<option value="Warwick">Warwick</option>
+				<option value="Wukong">Wukong</option>
+				<option value="Xerath">Xerath</option>
+				<option value="Xin Zhao">Xin Zhao</option>
+				<option value="Yorick">Yorick</option>
+				<option value="Zac">Zac</option>
+				<option value="Zed">Zed</option>
+				<option value="Ziggs">Ziggs</option>
+				<option value="Zilean">Zilean</option>
+				<option value="Zyra">Zyra</option>
+			</select>
 			</td>
 		</tr>
 		<tr><!--Row 2-->
+			<td>Did they call specific roles?</td>
 			<td>
 				<select name="position_dropdown_1">
-					<option value="Default">Default</option>
+					<option value="none">None</option>
 					<option value="Top">Top</option>
 					<option value="Middle">Middle</option>
 					<option value="Carry">AD Carry</option>
@@ -756,7 +766,7 @@ Select Champion by:
 			</td>
 			<td>
 				<select name="position_dropdown_2">
-					<option value="Default">Default</option>
+					<option value="none">None</option>
 					<option value="Top">Top</option>
 					<option value="Middle">Middle</option>
 					<option value="Carry">AD Carry</option>
@@ -766,7 +776,7 @@ Select Champion by:
 			</td>
 			<td>
 				<select name="position_dropdown_3">
-					<option value="Default">Default</option>
+					<option value="none">None</option>
 					<option value="Top">Top</option>
 					<option value="Middle">Middle</option>
 					<option value="Carry">AD Carry</option>
@@ -776,7 +786,7 @@ Select Champion by:
 			</td>
 			<td>
 				<select name="position_dropdown_4">
-					<option value="Default">Default</option>
+					<option value="none">None</option>
 					<option value="Top">Top</option>
 					<option value="Middle">Middle</option>
 					<option value="Carry">AD Carry</option>
@@ -786,7 +796,7 @@ Select Champion by:
 			</td>
 			<td>
 				<select name="position_dropdown_5">
-					<option value="Default">Default</option>
+					<option value="none">None</option>
 					<option value="Top">Top</option>
 					<option value="Middle">Middle</option>
 					<option value="Carry">AD Carry</option>
@@ -795,15 +805,50 @@ Select Champion by:
 				</select>
 			</td>
 		</tr>
+		<tr>
+			<td>
+				<h2>I want champs that: </h2>
+			</td>
+		</tr>
 		<tr><!--Row 3-->
 			<td>
-				<input type="submit" value="Search">
+				Have hard CC <br>
+				Are Easy to Play <br>
+				Work well with my team <br>
+			</td>
+			<td>
+				<input type="checkbox" name="check_list[]" value="value 1"> <br>
+				<input type="checkbox" name="check_list[]" value="value 1"> <br>
+				<input type="checkbox" name="check_list[]" value="value 1"> <br>
+			</td>
+		</tr>
+		<tr><!--Row 4-->
+			<td>
+				<input type="submit" value="What champion should I play?">
 			</td>
 		</tr>
 	</table>
 </form>
 
-<iframe width="260" height="500" name="resultsFrame" frameborder="0">I never LOSE</iframe>
+<table>
+	<tr>
+		<td>
+			<h3>Details</h3>
+		</td>
+		<td>
+			<h3>Results</h3>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<iframe width="500" height="200" name="detailsFrame" frameborder="1"></iframe>
+		</td>
+		<td>
+			<iframe width="500" height="200" name="resultsFrame" frameborder="1"></iframe>
+		</td>
+	</tr>
+</table>
+
 
 </body>
 </html>
