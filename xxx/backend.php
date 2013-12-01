73a10a2da88362ee6685fbfc03436588aabf4a57
ryanDalain <sackohammers1992@gmail.com>
2013-12-01 01:46:07 -0500

    backend page added

---
+++ xxx/backend.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<a href="index.php">Main page (For Users)</a> <br> <br>
+
+<a href="modify_champ.php">Modify Champ Table</a> <br> <br>
+<a href="modify_position.php">Modify Position Table</a> <br> <br>
+<a href="modify_role.php">Modify Role Table</a> <br> <br>
+<a href="modify_www.php">Modify WWW Table</a> <br> <br>
+<a href="modify_cc.php">Modify CC Table</a> <br> <br>
+
+<!--
+
+<br> <br> <a href="backend.php">Backend page</a> <br> <br>
+
+-->
+</body>
+</html>
