912963e5d26d5781f27dff04ef1a4822acad834d
James Kobayashi <kobayashijames@gmail.com>
2013-12-01 01:42:56 -0600

    modify www added

---
+++ xxx/modify_www.php
@@ -0,0 +1,132 @@
+<html>
+<body>
+
+<html>
+<body>
+
+        <?php
+        
+        $db_host = "localhost";
+        $db_username = "root";
+        $db_pass = "";
+        $db_name = "champ select";
+        
+        @mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
+        @mysql_select_db("$db_name") or die ("DB not found");
+		
+		
+		$queryOriginal = "SELECT Name FROM champion";
+		
+		$query_data1 = mysql_query($queryOriginal) or die("Could not find Champion table");
+        	$query_data2 = mysql_query($queryOriginal) or die("Could not find Champion table");
+        	$query_data3 = mysql_query($queryOriginal) or die("Could not find Champion table");
+        	$query_data4 = mysql_query($queryOriginal) or die("Could not find Champion table");
+        
+        ?>
+
+<br>
+
+<form action="modify_www_results.php" method="post">
+
+<table>
+	<tr>
+		<td>
+			Select a Champion from the list:
+		</td>
+
+		<td>
+			<select name="champ_dropdown_1">
+			<?php
+
+				while($info = mysql_fetch_array( $query_data1 )) 
+				{
+					Print "<option value = \"{$info['Name']}\">{$info['Name']}</option>";
+				}
+
+			?>
+			</select>
+		</td>
+	</tr>
+</table>
+
+<br>
+
+<table>
+
+	<tr>
+		<td>
+			Select a Champion to ADD a pairing:
+		</td>
+
+		<td>
+			<select name="champ_dropdown_2">
+			<?php
+
+				while($info = mysql_fetch_array( $query_data2 )) 
+				{
+					Print "<option value = \"{$info['Name']}\">{$info['Name']}</option>";
+				}
+
+			?>
+			</select>
+		</td>
+	</tr>	
+</table>
+
+<input type="submit" name="modify" value="Add new pair"> <br>
+
+<br>
+
+<table>
+	<tr>
+		<td>
+			Select a Champion to DELETE or MODIFY:
+		</td>
+
+		<td>
+			<select name="champ_dropdown_3">
+			<?php
+
+				while($info = mysql_fetch_array( $query_data3 )) 
+				{
+					Print "<option value = \"{$info['Name']}\">{$info['Name']}</option>";
+				}
+
+			?>
+			</select>
+		</td>
+	</tr>
+</table>
+
+<input type="submit" name="modify" value="Delete pair"> <br>
+
+<br>
+
+<table>
+	<tr>
+		<td>
+			Select a Champion to MODIFY the above:
+		</td>
+
+		<td>
+			<select name="champ_dropdown_4">
+			<?php
+
+				while($info = mysql_fetch_array( $query_data4 )) 
+				{
+					Print "<option value = \"{$info['Name']}\">{$info['Name']}</option>";
+				}
+
+			?>
+			</select>
+		</td>
+	</tr>	
+</table>
+
+
+<input type="submit" name="modify" value="Modify pair"> <br>
+</form>
+
+
+</body>
+</html>
---
+++ xxx/modify_www_results.php
@@ -0,0 +1,47 @@
+
+<?php
+	
+	$db_host = "localhost";
+	$db_username = "root";
+	$db_pass = "";
+	$db_name = "champ select";
+	
+	@mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
+	@mysql_select_db("$db_name") or die ("DB not found");
+	
+	switch($_POST["modify"])
+	{
+		case "Add new pair":
+
+			// Copy fields into local variables
+			$Champ1 = $_POST["champ_dropdown_1"];
+			$Champ2 = $_POST["champ_dropdown_2"];
+			
+			$modify_query = "INSERT INTO `www`(`Champ1`, `Champ2`) VALUES ('{$Champ1}','{$Champ2}');";			
+
+			break;
+
+		case "Delete pair":
+
+			$Champ1 = $_POST["champ_dropdown_1"];
+			$Champ3 = $_POST["champ_dropdown_3"];
+	
+			$modify_query = "DELETE FROM `www` WHERE Champ1 = '" . $Champ1 . "' AND Champ2 = '" . $Champ3 . "';";
+
+			break;
+
+		case "Modify pair":
+
+			$Champ1 = $_POST["champ_dropdown_1"];
+			$Champ3 = $_POST["champ_dropdown_3"];			
+			$Champ4 = $_POST["champ_dropdown_4"];
+	
+			$modify_query = "UPDATE `www` SET Champ1= '" . $Champ1 . "', Champ2= '" . $Champ4 . "' WHERE Champ1 = '" . $Champ1 . "' AND Champ2 = '" . $Champ3 . "';";
+
+			break;
+	
+	}
+	
+	$query_data = mysql_query($modify_query) or die("Could not modify pair<br>");
+	
+?>
